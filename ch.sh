fd8c6dbabd74a96f06414f497e67d668949ca563
techrayyan <techrayyan002@gmail.com>
2025-11-13 14:11:39 -0500

    Update ch.sh

--- ch.sh
+++ ch.sh
@@ -17,7 +17,7 @@
 : '
 MIT License
 
-Copyright (c) 2022 techrayyan
+Copyright (c) 2025 techrayyan
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
